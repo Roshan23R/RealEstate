28c8b8c736f4995429a423686a881822e5944059
Rkesh Roushan <rakeshroushan2341@gmail.com>
2023-10-23 19:06:56 +0530

    create a test api rote

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';    
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
+
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
@@ -14,3 +16,6 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is listening on port 3000!');
 })
+
+app.use("/api/user", userRouter);
+
